ddbe2cb352d282da02892899fcb489f01d5ad2f2
ashborn04 <162012466+ashborn04@users.noreply.github.com>
2024-06-08 18:49:17 +0530

    Done some tuning and added movement buttons

--- Assets/Scripts/BottleFlipper.cs
+++ Assets/Scripts/BottleFlipper.cs
@@ -1,5 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
+// using UnityEngine;
+
+// public class BottleFlipper : MonoBehaviour
+// {
+//     public float flipForce = 10f;
+//     public float jumpForce = 5f;
+//     private bool isLeftClicked = false;
+//     private bool isRightClicked = false;
+//     private float clickTimeThreshold = 0.5f; // Time threshold for double-click
+//     private float lastClickTime = 0f;
+//     AudioManager audioManager;
+//      private void Awake()
+//     {
+//         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+//     }
+
+//     void Update()
+//     {
+        
+//         if (Input.GetMouseButtonDown(0)) // Left mouse button
+//         {
+            
+//             if (Time.time - lastClickTime <= clickTimeThreshold)
+//             {
+//                 if (!isLeftClicked)
+//                 {
+//                     isLeftClicked = true;
+//                     FlipBottleLeft();
+//                 }
+//                 else
+//                 {
+//                     isLeftClicked = false;
+//                 }
+//             }
+//             else
+//             {
+//                 isLeftClicked = false;
+//             }
+//             lastClickTime = Time.time;
+//         }
+//         else if (Input.GetMouseButtonDown(1)) 
+//         // Right mouse button
+//         {
+            
+//             if (Time.time - lastClickTime <= clickTimeThreshold)
+//             {
+//                 if (!isRightClicked)
+//                 {
+//                     isRightClicked = true;
+//                     FlipBottleRight();
+//                 }
+//                 else
+//                 {
+//                     isRightClicked = false;
+//                 }
+//             }
+//             else
+//             {
+//                 isRightClicked = false;
+//             }
+//             lastClickTime = Time.time;
+//         }
+//     }
+
+//     void FlipBottleLeft()
+//     {
+//         audioManager.PlaySFX(audioManager.flipping);
+//         GetComponent<Rigidbody2D>().AddTorque(flipForce, ForceMode2D.Impulse);
+//         GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+//         GetComponent<Rigidbody2D>().AddForce(Vector2.left * flipForce, ForceMode2D.Impulse);
+//     }
+
+//     void FlipBottleRight()
+//     {
+//         audioManager.PlaySFX(audioManager.flipping);
+//         GetComponent<Rigidbody2D>().AddTorque(-flipForce, ForceMode2D.Impulse);
+//         GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+//         GetComponent<Rigidbody2D>().AddForce(Vector2.right * flipForce, ForceMode2D.Impulse);
+//     }
+// }
+
 using UnityEngine;
 
 public class BottleFlipper : MonoBehaviour
@@ -11,17 +92,16 @@ public class BottleFlipper : MonoBehaviour
     private float clickTimeThreshold = 0.5f; // Time threshold for double-click
     private float lastClickTime = 0f;
     AudioManager audioManager;
-     private void Awake()
+
+    private void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
 
     void Update()
     {
-        
-        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            
             if (Time.time - lastClickTime <= clickTimeThreshold)
             {
                 if (!isLeftClicked)
@@ -40,10 +120,8 @@ public class BottleFlipper : MonoBehaviour
             }
             lastClickTime = Time.time;
         }
-        else if (Input.GetMouseButtonDown(1)) 
-        // Right mouse button
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            
             if (Time.time - lastClickTime <= clickTimeThreshold)
             {
                 if (!isRightClicked)
@@ -64,7 +142,7 @@ public class BottleFlipper : MonoBehaviour
         }
     }
 
-    void FlipBottleLeft()
+    public void FlipBottleLeft()
     {
         audioManager.PlaySFX(audioManager.flipping);
         GetComponent<Rigidbody2D>().AddTorque(flipForce, ForceMode2D.Impulse);
@@ -72,7 +150,7 @@ public class BottleFlipper : MonoBehaviour
         GetComponent<Rigidbody2D>().AddForce(Vector2.left * flipForce, ForceMode2D.Impulse);
     }
 
-    void FlipBottleRight()
+    public void FlipBottleRight()
     {
         audioManager.PlaySFX(audioManager.flipping);
         GetComponent<Rigidbody2D>().AddTorque(-flipForce, ForceMode2D.Impulse);
--- Assets/Scripts/BottleMover.cs
+++ Assets/Scripts/BottleMover.cs
@@ -2,28 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// public class BottleMover : MonoBehaviour
+// {
+//     public float moveSpeed = 5f;
+
+//     void Update()
+//     {
+//         if (Input.GetMouseButtonDown(0)) // Left mouse button
+//         {
+//             MoveBottleLeft();
+//         }
+//         else if (Input.GetMouseButtonDown(1)) // Right mouse button
+//         {
+//             MoveBottleRight();
+//         }
+//     }
+
+//     void MoveBottleLeft()
+//     {
+//         transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+//     }
+
+//     void MoveBottleRight()
+//     {
+//         transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+//     }
+// }
+
+
 public class BottleMover : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             MoveBottleLeft();
         }
-        else if (Input.GetMouseButtonDown(1)) // Right mouse button
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             MoveBottleRight();
         }
     }
 
-    void MoveBottleLeft()
+    public void MoveBottleLeft()
     {
         transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
     }
 
-    void MoveBottleRight()
+    public void MoveBottleRight()
     {
         transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
     }
